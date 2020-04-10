304e1f440d4a1de14f8b09882cd7b3a6551a428a
Jessica Brunhara <brunharaj@gmail.com>
2020-04-10 09:59:32 -0300

    ultima versão

--- src/cipher.js
+++ src/cipher.js
@@ -1,34 +1,36 @@
 const cipher = {
   encode: function(deslocamentoOriginal, textoOriginal) {
-    const texto = textoOriginal.toUpperCase();
-    const deslocamento = parseInt(deslocamentoOriginal)
+    const texto = textoOriginal.toUpperCase(); 
+    const deslocamento = parseInt(deslocamentoOriginal) 
 
-    let indiceAtual = 0;
-    let codigos = [];
-    while(indiceAtual < texto.length) {
+    let indiceAtual = 0; 
+    let codigos = []; 
+    while(indiceAtual < texto.length) { 
       const codigoDoCaractere = texto.charCodeAt(indiceAtual);
       codigos.push(codigoDoCaractere);
-      indiceAtual = indiceAtual + 1;
+    } 
+ 
+
+    indiceAtual = 0; 
+    let codigosFinais = []; 
+    while(indiceAtual < codigos.length) { 
+      const codigoDeslocado = codigos[indiceAtual] + deslocamento; 
+      codigosFinais.push(((codigoDeslocado -65) % 26) + 65); 
+      indiceAtual = indiceAtual + 1; 
     }
 
+
+   return String.fromCharCode(...codigosFinais);
+  },
+
+  decode: function (deslocamentoOriginal, textoOriginal) {
     
 
-    indiceAtual = 0;
-    let codigosFinais = [];
-    while(indiceAtual < codigos.length) {
-      const codigoDeslocado = codigos[indiceAtual] + deslocamento;
-      codigosFinais.push(((codigoDeslocado -64) % 26) + 64);
-      indiceAtual = indiceAtual + 1;
-      
-    }
 
 
-    return String.fromCharCode(...codigosFinais);
-
-  },
-  decode: function (deslocamentoOriginal, textoOriginal) {
     return texto;
   }
+  
 };
 
 export default cipher;
@@ -42,4 +44,3 @@ export default cipher;
 
 
   // ...//
-
--- src/index.js
+++ src/index.js
@@ -1,32 +1,31 @@
 import cipher from './cipher.js';
 
-
-
 const campoTextoEscrever = document.getElementById('textoEscrever');
 const campoTextoResultado = document.getElementById('textoResultado');
 const campoDeslocamento = document.getElementById('deslocamento');
 const camposEscolha = document.getElementsByName('escolhaRadio');
 
+
+
 function processarDados(event) {
     event.preventDefault();
 
 
-    const textoEscrever = campoTextoEscrever.value;
-    const deslocamento = campoDeslocamento.value;
-
-
+   const textoEscrever = campoTextoEscrever.value
+   const deslocamento = campoDeslocamento.value
+   
+   
     if (camposEscolha[0].checked) {
-        campoTextoResultado.value = cipher.encode(deslocamento, textoEscrever);
-        
-        
+        campoTextoResultado.innerHTML = cipher.encode(deslocamento, textoEscrever);
 
     }
 
     if (camposEscolha[1].checked) {
-        
+    
     }
 
 }
 
 const formulario = document.getElementById('escolha');
 formulario.onsubmit = processarDados;
+
