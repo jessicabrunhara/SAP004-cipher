12af1674b805273a30ba4294ed3eb9265dfe226d
Jessica Brunhara <brunharaj@gmail.com>
2020-04-10 15:08:11 -0300

    Updates

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+const campoTextoEscrever = document.getElementById('textoEscrever');
+const campoTextoResultado = document.getElementById('textoResultado');
+const campoDeslocamento = document.getElementById('deslocamento');
+const camposEscolha = document.getElementsByName('escolhaRadio');
+
+
+
+function processarDados(event) {
+    event.preventDefault();
+
+
+   const textoEscrever = campoTextoEscrever.value
+   const deslocamento = campoDeslocamento.value
+   
+   
+    if (camposEscolha[0].checked) {
+        campoTextoResultado.innerHTML = cipher.encode(deslocamento, textoEscrever);
+
+    }
+
+    if (camposEscolha[1].checked) {
+    
+    }
+
+}
+
+const formulario = document.getElementById('escolha');
+formulario.onsubmit = processarDados;
+
